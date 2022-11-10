b63d07079af74a485eadfc17097dc93fe2e339e0
Dalton <magetadally19@gmail.com>
2022-11-10 15:13:15 +0300

    Full Text

--- domestic.calculator.php
+++ domestic.calculator.php
@@ -92,9 +92,45 @@
                     "Maximum years for Comprehensive cover is 12 years (2010)"
                 </small>
             </div>
+            <div class="tw-w-full md:tw-w-1/2 tw-mt-6 md:tw-mt-0">
+                <div id="amount" type="text" name="amount" class="mx-4">
+                    <div class="v-input theme--light v-text-field v-text-field--prefix v-text-field--is--booted">
+                        <div class="v-input__control">
+                            <div class="v-input__slot white">
+                                <div class="v-text-field__slot">
+                                    <label for="amount-input" class="v-label theme--light" style="left:28px; right:auto; position:absolute;">Value of Vehicle</label>
+                                    <div class="v-text-field__prefix">Kes</div>
+                                    <input id="amount-input" type="text">
+                                </div>
+                            </div>
+                            <div class="v-text-field__details">
+                                <div class="v-messages theme--light error--text" role="alert">
+                                    <div class="v-message__wrapper">
+                                        <div class="v-message__message">Value of car field is required</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <small class="c-yellow-text tw-italic">
+                    <i aria-hidden="true" class="v-icon notranslate mdi mdi-information-outline theme--light secondary--text" style="font-size: 14px;"></i>
+                    "Minimum value for Comprehensive cover is KES 600,000"
+                </small>
+            </div>
+        </div>
+        <div class="v-card__actions col-sm-6 tw-mt-5 offset-sm-3">
+            <button type="button" disabled="disabled" class="my-3 v-btn v-btn--block v-btn--has-bg theme--light v-size--large" id="submit">
+                <span class="v-btn__content"> Get Quote </span>
+            </button>
+        </div>
     </form>
-
 </div>
+<div>
+    <div role="dialog" class="v-dialog__container"> </div>
+</div>
+<div id="fc_frame" class="fc-widget-normal" style="bottom: 15px; right: 15px;"></div>
+    <iframe id="fc_widget" name="fc_widget" title="Chat" frameborder="0" allowfullscreen="true" webkitallowfullscreeen="true" mozallowfullscreen="true"></iframe>
 
     <!-- =====================================FOOTER===================================== -->
 
